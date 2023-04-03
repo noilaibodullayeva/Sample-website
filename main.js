e4d3bfd9982af89c993630d6b596e9f96b42469a
noiaibodullayeva <ibodullaevanoila07@gmail.com>
2023-04-03 12:30:40 +0500

    responsive website

---
+++ main.js
@@ -0,0 +1,12 @@
+let Age = prompt("How old are u?", "");
+ 
+
+if (Age >= 18) {
+   alert ("You can see this webpage",  " ");
+} else if (Age<=17) {
+  alert ("Please try again when you are at least 18", " ");
+} else if (Age>60) {
+ alert ("You are too old to continue", " ");
+} else {
+  ("Try again later", " ");
+}
